f7d8ba9f0b8550c70250f4b694e9b2d5e05ad751
Rafael Souza <rafael.jose@ferreri.co>
2024-06-04 23:51:09 -0300

    add register screen

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthProvider, useAuth } from './src/contexts/AuthContext';
 import { Login } from './src/screens/Login';
 import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
+import { RootStackParamList } from './src/@types/navigation';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
@@ -23,7 +25,12 @@ function Layout() {
       <Stack.Navigator>
         {authState?.authenticated ?
           <Stack.Screen name='Home' component={Home} /> :
-          <Stack.Screen name='Login' component={Login} />
+          (
+            <>
+              <Stack.Screen name='Login' component={Login} />
+              <Stack.Screen name='Register' component={Register} />
+            </>
+          )
         }
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
-import { z } from 'zod';
 
+import { z } from 'zod';
 import { useAuth } from '../contexts/AuthContext';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { RootStackParamList } from '../@types/navigation';
 
 const emailSchema = z.string().email({ message: "Email inválido" });
 const passwordSchema = z.string().min(8, { message: "A senha deve ter pelo menos 8 caracteres" })
@@ -12,39 +10,57 @@ const passwordSchema = z.string().min(8, { message: "A senha deve ter pelo menos
   .regex(/[a-z]/, { message: "A senha deve conter pelo menos uma letra minúscula" })
   .regex(/[0-9]/, { message: "A senha deve conter pelo menos um número" })
   .regex(/[^a-zA-Z0-9]/, { message: "A senha deve conter pelo menos um caractere especial" });
+const nameSchema = z.string().min(1, { message: "Nome completo é obrigatório" });
 
-export function Login() {
+export function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-  const { onLogin } = useAuth();
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const [nameError, setNameError] = useState('');
+  const { onRegister, onLogin } = useAuth();
 
-  const handleLogin = () => {
+  const handleSignup = async () => {
     const emailValidation = emailSchema.safeParse(email);
     const passwordValidation = passwordSchema.safeParse(password);
+    const nameValidation = nameSchema.safeParse(name);
 
-    // if (!emailValidation.success) {
-    //   setEmailError(emailValidation.error.errors[0].message);
-    // } else {
-    //   setEmailError('');
-    // }
+    if (!emailValidation.success) {
+      setEmailError(emailValidation.error.errors[0].message);
+    } else {
+      setEmailError('');
+    }
 
-    // if (!passwordValidation.success) {
-    //   setPasswordError(passwordValidation.error.errors[0].message);
-    // } else {
-    //   setPasswordError('');
-    // }
+    if (!passwordValidation.success) {
+      setPasswordError(passwordValidation.error.errors[0].message);
+    } else {
+      setPasswordError('');
+    }
 
-    console.log(email)
-    console.log(password)
+    if (!nameValidation.success) {
+      setNameError(nameValidation.error.errors[0].message);
+    } else {
+      setNameError('');
+    }
 
-    onLogin!(email, password);
+    await onRegister(email, password)
+    
+    onLogin(email, password)
   };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.label}>Nome Completo</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Digite seu nome completo"
+        autoCapitalize="words"
+      />
+      {nameError ? <Text style={styles.errorText}>{nameError}</Text> : null}
+      
       <Text style={styles.label}>Email</Text>
       <TextInput
         style={styles.input}
@@ -67,11 +83,7 @@ export function Login() {
       />
       {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
       
-      <Button title="Entrar" onPress={handleLogin} />
-
-      <Button title="Criar conta" onPress={() => {
-        navigation.navigate('Register')
-      }} />
+      <Button title="Criar Conta" onPress={handleSignup} />
     </View>
   );
 };
