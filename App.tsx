5ce9d74c92ed7ceb07ce7f3856551341891ab532
Rafael Souza <rafael.jose@ferreri.co>
2024-06-25 22:01:03 -0300

    add bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,38 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { AuthProvider, useAuth } from './src/contexts/AuthContext';
-import { Login } from './src/screens/Login';
-import { Home } from './src/screens/Home';
-import { Register } from './src/screens/Register';
+import { AuthProvider } from './src/contexts/AuthContext';
 import { RootStackParamList } from './src/@types/navigation';
+import { Router } from './router';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <AuthProvider>
-      <Layout />
+      <Router />
     </AuthProvider>
   );
-}
-
-function Layout() {
-  const { authState } = useAuth();
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {authState?.authenticated ?
-          <Stack.Screen name='Home' component={Home} /> :
-          (
-            <>
-              <Stack.Screen name='Login' component={Login} />
-              <Stack.Screen name='Register' component={Register} />
-            </>
-          )
-        }
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
 }
---
+++ router.tsx
@@ -0,0 +1,39 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { useAuth } from "./src/contexts/AuthContext";
+import { Home } from "./src/screens/Home";
+import { Login } from "./src/screens/Login";
+import { Register } from "./src/screens/Register";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export function Router() {
+    const { authState } = useAuth();
+  
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          {authState?.authenticated ?
+            <Stack.Screen name='Tabs' component={BottomTabs} options={{ headerShown: false }} /> :
+            (
+              <>
+                <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name='Register' component={Register} />
+              </>
+            )
+          }
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+}
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  )
+}
