1596e7c75d364522bf80c4326d64bb77b505f05a
Rafael Souza <rafael.jose@ferreri.co>
2024-07-11 00:56:42 -0300

    prefetch user data

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,11 @@ import { StyleSheet, View } from 'react-native'
 
 import { JobList } from '../modules/jobs/JobList'
 import { JobFilter } from '../modules/jobs/JobFilter'
+import { useWhoami } from '../modules/users/stores'
 
 export function Home() {
+  useWhoami()
+
   return (
     <View style={styles.container}>
       <JobFilter />
