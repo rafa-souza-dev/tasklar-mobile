c06728d5b8ae14926b63ffdf676f5cf01b6b7b0d
Rafael Souza <rafael.jose@ferreri.co>
2024-07-13 23:52:15 -0300

    fn: sumTime

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
 import { getDay, addWeeks, differenceInDays, differenceInWeeks } from 'date-fns'
 
-import { generateWeekOfDay } from './date'
+import { generateWeekOfDay, sumTime } from './date'
 
 describe('generateWeekOfDay()', () => {
   describe('with current date', () => {
@@ -34,6 +34,48 @@ describe('generateWeekOfDay()', () => {
 
       expect(differenceInDays(nextWeek[0], currentWeek[0])).toBe(7)
       expect(differenceInWeeks(nextWeek[0], currentWeek[0])).toBe(1)
+    })
+  })
+})
+
+describe('sumTime()', () => {
+  describe('when duration has no minutes', () => {
+    describe('when time has no minutes', () => {
+      const time = '11:00'
+      const duration = '1hr'
+
+      it('should be able to sum time with duration', () => {
+        expect(sumTime(time, duration)).toBe('12:00')
+      })
+    })
+
+    describe('when time has minutes', () => {
+      const time = '11:30'
+      const duration = '1hr'
+
+      it('should be able to sum time with duration', () => {
+        expect(sumTime(time, duration)).toBe('12:30')
+      })
+    })
+  })
+
+  describe('when duration has minutes', () => {
+    describe('when time has no minutes', () => {
+      const time = '11:00'
+      const duration = '1hr30min'
+
+      it('should be able to sum time with duration', () => {
+        expect(sumTime(time, duration)).toBe('12:30')
+      })
+    })
+
+    describe('when time has minutes', () => {
+      const time = '10:45'
+      const duration = '2hr30min'
+
+      it('should be able to sum time with duration', () => {
+        expect(sumTime(time, duration)).toBe('13:15')
+      })
     })
   })
 })
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -16,3 +16,38 @@ export function generateWeekOfDay(date: Date): Date[] {
 
   return week
 }
+
+export function getTimesInInterval(
+  startTime: string,
+  endTime: string,
+  duration: string,
+) {}
+
+export function sumTime(time: string, duration: string): string {
+  const splitTime = time.split(':')
+  const timeHours = splitTime[0]
+  const timeMinutes = splitTime[1]
+  const durationSplitByHour = duration.split('hr')
+  const durationHours = durationSplitByHour[0]
+  const durationMinutes = durationSplitByHour[1].slice(0, 2)
+  let sumHours = Number(timeHours) + Number(durationHours)
+  let sumMinutes = Number(timeMinutes) + Number(durationMinutes)
+
+  if (sumMinutes >= 60) {
+    sumMinutes = sumMinutes - 60
+    sumHours += 1
+  }
+
+  const formattedSumHours = convertTime(sumHours)
+  const formattedSumMinutes = convertTime(sumMinutes)
+
+  return `${formattedSumHours}:${formattedSumMinutes}`
+}
+
+function convertTime(time: number): string {
+  if (time < 10) {
+    return `0${time}`
+  }
+
+  return String(time)
+}
